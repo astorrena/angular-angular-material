c4ef4567710a7f7be1750147dcdc0d843d34e317
asier <astorrena@gmail.com>
2025-01-28 11:45:05 -0300

    Renombrar archivos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'anguar-angular-material' title`, () => {
+  it(`should have the 'angular-angular-material' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('anguar-angular-material');
+    expect(app.title).toEqual('angular-angular-material');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, anguar-angular-material');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-angular-material');
   });
 });
